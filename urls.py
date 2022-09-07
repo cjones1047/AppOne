ff83f03f14654fafe3488ebd14584f75bce759c4
cjones1047 <cjmj1047@gmail.com>
2022-09-07 18:44:03 -0500

    Replaced 'url' imports with 're_path' imports due to 'url' crashing server, apparent deprecation the cause

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url 
+# from django.conf.urls import url
+from django.urls import re_path
 from AppOne import views
 
 urlpatterns=[
-    url(r'^department/$', views.departmentApi),
-    url(r'^department/([0-9]+)$', views.departmentApi),
+    re_path(r'^department/$', views.departmentApi),
+    re_path(r'^department/([0-9]+)$', views.departmentApi)
 ]
