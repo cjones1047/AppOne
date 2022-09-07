b640d563dd440cb99c910a823c88c4a7adb4d1eb
cjones1047 <cjmj1047@gmail.com>
2022-09-06 20:01:21 -0500

    Serializer code needed for models created

--- serializers.py
+++ serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+from AppOne.models import Departments, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ('DepartmentId',
+                'DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId',
+                'EmployeeName',
+                'Department',
+                'DateOfJoining',
+                'PhotoFileName')
