9685c8bc8dac74ee269e58a19cd92b49962e3302
cjones1047 <cjmj1047@gmail.com>
2022-09-07 20:02:47 -0500

    Added employee methods to AppOne/views.py, added employee urls to AppOne/urls.py

--- urls.py
+++ urls.py
@@ -4,5 +4,7 @@ from AppOne import views
 
 urlpatterns=[
     re_path(r'^department/$', views.departmentApi),
-    re_path(r'^department/([0-9]+)$', views.departmentApi)
+    re_path(r'^department/([0-9]+)$', views.departmentApi),
+    re_path(r'^employee/$', views.departmentApi),
+    re_path(r'^employee/([0-9]+)$', views.departmentApi)
 ]
